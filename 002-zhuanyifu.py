97359f3071798e2223982bd0f42f2a75a79e075e
Jack9988 <34261395+Jack9988@users.noreply.github.com>
2017-12-05 15:44:24 +0800

    Update 002-zhuanyifu.py

--- 002-zhuanyifu.py
+++ 002-zhuanyifu.py
@@ -1,8 +1,8 @@
 #转义符
-    print('I\'m ok.')
-    print('I\'m learning\nPython.')
-    print('\\\n\\')  #一个\，一个换行，一个\
-    print(r'\\\t\\') #使用r整体转
-    print('''line1
+print('I\'m ok.')
+print('I\'m learning\nPython.')
+print('\\\n\\')  #一个\，一个换行，一个\
+print(r'\\\t\\') #使用r整体转
+print('''line1
 line2
 line3''') #多行内容
