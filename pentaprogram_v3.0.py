92b374b2e1d9ee78061533ac2f8a5f373c6f2b5e
Jack9988 <34261395+Jack9988@users.noreply.github.com>
2017-12-05 15:56:45 +0800

    Add files via upload

---
+++ pentaprogram_v3.0.py
@@ -0,0 +1,49 @@
+"""
+功能：五角星的绘制
+版本：v3.0
+作者：杰克
+时间：2017年11月16日23:21:32
+新增功能：增加循环操作
+新增功能：使用迭代函数绘制
+
+"""
+
+import turtle
+
+def draw_recursive_pentagram(size):
+    """
+    迭代绘制,递归函数，自身调用自身来改变参数值。
+    :param size:
+    :return:
+    """
+    #计数器
+    count = 1
+    while count <= 5:
+        turtle.forward(size)
+        turtle.right(144)
+        count += 1
+
+    #五角星绘制完成,更新参数
+    size+=10
+    if size<=100:
+        draw_recursive_pentagram(size)
+
+def draw_pentagram(size):
+    """
+    绘制五角星
+    :param size:
+    :return:
+    """
+
+
+def main():
+    """
+        主函数
+    """
+    size=50
+    draw_recursive_pentagram(size)
+
+    turtle.exitonclick()
+
+if __name__=='__main__':
+    main()
